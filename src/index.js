b074750a00471f84b93dd1b0f21c401184d22917
Amirrk79 <amirhosseinkazemiseresht@aol.com>
2021-09-24 18:20:43 +0330

    fix bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import store from './Redux/store'
